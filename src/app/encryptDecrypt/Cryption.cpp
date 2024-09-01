e6b1847befbf15febd8864c30f809538af5e2ccf
tarun-kavipurapu <tarunkavipurapu@gmail.com>
2024-09-01 11:04:19 +0530

    first commit

---
+++ src/app/encryptDecrypt/Cryption.cpp
@@ -0,0 +1,40 @@
+#include "Cryption.hpp"
+#include "../processes/Task.hpp"
+#include "../fileHandling/ReadEnv.cpp"
+#include <ctime>
+#include <iomanip>
+
+int executeCryption(const std::string &taskData)
+{
+    Task task = Task::fromString(taskData);
+    ReadEnv env;
+    std::string envKey = env.getenv();
+    int key = std::stoi(envKey);
+    if (task.action == Action::ENCRYPT)
+    {
+        char ch;
+        while (task.f_stream.get(ch))
+        {
+            ch = (ch + key) % 256;
+            task.f_stream.seekp(-1, std::ios::cur);
+            task.f_stream.put(ch);
+        }
+        task.f_stream.close();
+    }
+    else
+    {
+        char ch;
+        while (task.f_stream.get(ch))
+        {
+            ch = (ch - key + 256) % 256;
+            task.f_stream.seekp(-1, std::ios::cur);
+            task.f_stream.put(ch);
+        }
+        task.f_stream.close();
+    }
+    std::time_t t = std::time(nullptr);
+    std::tm *now = std::localtime(&t);
+    std::cout << "Exiting the encryption/decryption at: " << std::put_time(now, "%Y-%m-%d %H:%M:%S") << std::endl;
+
+    return 0;
+}
